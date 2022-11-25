3908e34821f3815cb7cbae0de5dfd26649202d00
hendriramadhan <hendriramadhan1101@gmail.com>
2022-11-25 22:32:16 +0700

    finish destructuring function dan perbaikan alias dan default value

--- script.js
+++ script.js
@@ -30,9 +30,59 @@ console.log(brandHp, typeHp, tokoHp);
 
 //sesudah destructuring
 const {
-  brand: brandAlias, //alias
-  type = "type huwawei baru",
+  brand,
+  type,
   toko: { namaToko },
   ...dataLainnya
 } = smartphone;
-console.log(brandAlias, type, namaToko, dataLainnya);
+console.log(type, namaToko, dataLainnya);
+
+//destructuring function
+const dataImunisasi = {
+  namaAnak: "Hana",
+  namaOrtu: "Hendri",
+  beratBadan: "7kg",
+  alamat: {
+    desa: "LB terantang",
+    kec: "guntor",
+  },
+};
+
+//sebelum desctructuring
+// function printImunisasi(namaAnak, namaOrtu, beratBadan, alamat) {
+//   console.log(namaAnak, namaOrtu, beratBadan, alamat);
+// }
+// printImunisasi(
+//   dataImunisasi.namaOrtu,
+//   dataImunisasi.namaAnak,
+//   dataImunisasi.beratBadan,
+//   dataImunisasi.alamat
+// );
+
+//sesudah destructuring
+function printImunisasi({ namaAnak, namaOrtu, beratBadan, alamat }) {
+  console.log(namaAnak, namaOrtu, beratBadan, alamat);
+}
+printImunisasi(dataImunisasi);
+
+//alias dan default value
+const dataMahasiswa = {
+  nama: "Hendri Ramadhan,",
+  prodi: "Teknik Informatika,",
+  jenisKelamin: "Laki-laki,",
+  umur: 25,
+  alamat: {
+    desa: "Lb Terentang,",
+    kec: "Guntor",
+    kab: "Kuansing",
+  },
+};
+
+const {
+  nama: namaLengkap, //alias
+  prodi,
+  fakultas = "Teknik,", //default value
+  jenisKelamin,
+  ...dst
+} = dataMahasiswa;
+console.log(namaLengkap, prodi, fakultas, jenisKelamin, dst);
